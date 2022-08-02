1f55366baf4e39176fae772f27fd8d9a70aa2073
Akhlaq <imakhlaqXD@gmail.com>
2022-08-02 14:29:27 +0530

    array

---
+++ pointer.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// void fun(int *ptr){
+//     *ptr=*ptr+5;
+
+// }
+
+
+// int main(){
+//     int x=10;
+
+//     fun(&x);
+//     cout<<x;
+//     return 0;
+
+// }
+
+
+// void fun(string *s){
+//     cout<<*s;
+
+// }
+
+
+// int main(){
+//      string s="geeksForgeeks";
+//     fun( &s);
+//     cout<<s;
+//     return 0;
+
+// }
+
+
+
+
+
+// int main(){
+//     int arr[]={10,20,39,65,565,43};
+//     int *ptr=arr;
+//      ptr--;
+//     ptr++;
+
+//     cout<<sizeof(arr)<<endl;
+//     cout<<*ptr;
+
+// }
+
